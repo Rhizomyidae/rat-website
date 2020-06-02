a79008af3ffecb7eecff8ec468bc8c1abdb89951
火星人 <450216797@qq.com>
2020-06-03 00:06:18 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header />
     <router-view />
+    <Footer />
+
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default {
+  components: {
+    Footer,
+    Header
+  }
+};
+</script>
 
 <style>
 #app {
